663eccd26ff46bdc8ad39e560c6fe7574617ccd6
abhinav-js <74096521+abhinav-js@users.noreply.github.com>
2020-12-10 19:17:30 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,31 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        this.pointB = pointB;
+        this.bodyA = bodyA;
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(newBodyA) {
+        this.sling.bodyA = newBodyA;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
